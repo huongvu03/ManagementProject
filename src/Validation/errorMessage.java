de89c232c2762d988b877e6f4b8812f20876ce02
huongvu03 <huongvu031096@gmail.com>
2024-06-16 23:46:55 +0700

    validate package

---
+++ src/Validation/errorMessage.java
@@ -0,0 +1,22 @@
+
+package Validation;
+
+public class errorMessage {
+    private String errorMsg1 ="không được để trống";
+    private String errorMsg2 ="chỉ nhập số";
+    private String errorMsg3 ="format khác";
+
+    public String getErrorMsg1() {
+        return errorMsg1;
+    }
+
+    public String getErrorMsg2() {
+        return errorMsg2;
+    }
+
+    public String getErrorMsg3() {
+        return errorMsg3;
+    }
+    
+    
+}
